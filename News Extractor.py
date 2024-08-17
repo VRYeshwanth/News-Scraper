379f3b6b1a45ad8b6591617d3bd2e0803a49f088
VR Yeshwanth <vryeshwanth06@gmail.com>
2024-08-17 16:12:34 +0530

    Error Message during absence of internet
    Added error messages to inform user about absence of internet.

--- News Extractor.py
+++ News Extractor.py
@@ -11,9 +11,9 @@ window.resizable(0,0)
 def generate_news():
     output.delete("1.0", tk.END)
     world_news_url = "https://www.indiatoday.in/world"
-    world_page = requests.get(world_news_url)
-    world_soup = BeautifulSoup(world_page.text, "html.parser")
-    if world_page.status_code == 200:
+    try:
+        world_page = requests.get(world_news_url)
+        world_soup = BeautifulSoup(world_page.text, "html.parser")
         wor_div = world_soup.find("div", class_ = "story__grid")
         w_article = wor_div.find_all("article")
         w_headlines = []
@@ -22,15 +22,15 @@ def generate_news():
         output.insert(tk.END, "International News :-\n")
         for i, headline in enumerate(w_headlines):
             output.insert(tk.END, f"{i+1}) {headline}\n")
-    else:
-        messagebox.showerror("ERROR", "We had a problem fetching International News")
+    except Exception:
+        messagebox.showerror("ERROR", f"We had a problem fetching International News")
     
     output.insert(tk.END, "\n")
 
     india_news_url = "https://www.indiatoday.in/india"
-    india_page = requests.get(india_news_url)
-    india_soup = BeautifulSoup(india_page.text, "html.parser")
-    if india_page.status_code == 200:
+    try:
+        india_page = requests.get(india_news_url)
+        india_soup = BeautifulSoup(india_page.text, "html.parser")
         ind_div = india_soup.find("div", class_ = "story__grid")
         i_article = ind_div.find_all("article")
         i_headlines = []
@@ -39,15 +39,15 @@ def generate_news():
         output.insert(tk.END, "National News :-\n")
         for i, headline in enumerate(i_headlines):
             output.insert(tk.END, f"{i+1}) {headline}\n")
-    else:
+    except Exception:
         messagebox.showerror("ERROR", "We had a problem fetching India News")
     
     output.insert(tk.END, "\n")
 
     business_news_url = "https://www.indiatoday.in/business"
-    business_page = requests.get(business_news_url)
-    business_soup = BeautifulSoup(business_page.text, "html.parser")
-    if business_page.status_code == 200:
+    try:
+        business_page = requests.get(business_news_url)
+        business_soup = BeautifulSoup(business_page.text, "html.parser")
         bus_div = business_soup.find("div", class_ = "story__grid")
         b_article = bus_div.find_all("article")
         b_headlines = []
@@ -56,7 +56,7 @@ def generate_news():
         output.insert(tk.END, "Business News :-\n")
         for i, headline in enumerate(b_headlines):
             output.insert(tk.END, f"{i+1}) {headline}\n")
-    else:
+    except Exception:
         messagebox.showerror("ERROR", "We had a problem fetching Business News")
     
 
